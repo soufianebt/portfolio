66becf27ed89890793c581ab29ad5a09ac056667
Faouzi Mohamed <me@mfaouzi.com>
2023-04-09 17:13:57 +0000

    feat(layout): update of the current layout to support responsiveness (mobile first approach)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterLink, RouterView} from 'vue-router'
 </script>
 
 <template>
-  <div class="main-container">
-      <div class="container">
-        <section class="main-container__left-side">
-        <img src="/soufianeBoutahiri.jpg" alt="Soufiane Boutahiri PhotoS" >
-        <h1 >Soufiane BOUTAHIRI</h1>
-        <nav >
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-            <RouterLink to="/education">Education</RouterLink>
-            <RouterLink to="/technologies">Technologies</RouterLink>
-            <RouterLink to="/projects">Projects</RouterLink>
-            <RouterLink to="/contacts">Contacts</RouterLink>
-          </nav>
-      </section>
-      </div>
-    <div class="container">
-    <section class="main-container__right-side">
-      <RouterView />
-    </section>
+    <div class="main-container">
+        <div class="container left-side">
+            <section class="main-container__left-side">
+                <div class="avatar-container">
+                    <img alt="Soufiane Boutahiri PhotoS" height="120" src="/soufianeBoutahiri.jpg" width="120">
+                </div>
+                <h1>Soufiane BOUTAHIRI</h1>
+                <nav>
+                    <RouterLink to="/">Home</RouterLink>
+                    <RouterLink to="/about">About</RouterLink>
+                    <RouterLink to="/education">Education</RouterLink>
+                    <RouterLink to="/technologies">Technologies</RouterLink>
+                    <RouterLink to="/projects">Projects</RouterLink>
+                    <RouterLink to="/contacts">Contacts</RouterLink>
+                </nav>
+            </section>
+        </div>
+        <div class="container">
+            <section class="main-container__right-side">
+                <RouterView/>
+            </section>
+        </div>
     </div>
-  </div>
 </template>
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="about">
-    <h1>About Me</h1>
-    <ul>
-      <li><span>Full Name :</span> Soufiane BOUTAHIRI </li>
-      <li><span>Age :</span> 24 </li>
-      <li><span>Qualification :</span> State Engineer (Bac +5) </li>
-      <li><span>Post :</span> Full-Stack Engineer Dot Net/ Vue js </li>
-      <li><span>Languages :</span> Arabic(Native), French(C1), English (B2) </li>
-    </ul>
-    <button @click="downloadFile">Download CV</button>
-  </div>
+    <div class="about">
+        <h1>About Me</h1>
+        <ul>
+            <li><span>Full Name :</span> Soufiane BOUTAHIRI</li>
+            <li><span>Age :</span> 24</li>
+            <li><span>Qualification :</span> State Engineer (Bac +5)</li>
+            <li><span>Post :</span> Full-Stack Engineer Dot Net/ Vue js</li>
+            <li><span>Languages :</span> Arabic(Native), French(C1), English (B2)</li>
+        </ul>
+        <button title="Click to download the Resume" @click="downloadFile">Download CV</button>
+    </div>
 </template>
 <script>
-import {CV_LINKS} from "../CONST/PUBLIC_Const.js";
+import {CV_LINKS} from "@/CONST/PUBLIC_Const";
 
 export default {
   methods: {
     // TODO: Fix this function
-    downloadFile : function() {
+    downloadFile: function () {
       window.open(CV_LINKS.FRENCH, '_blank')
     }
   }
