2516ea5befad8a596654e4df920c91b1ecaca619
soufianebt <soufiane.boutahiri@um5.net.ma>
2023-04-09 13:56:38 +0000

    Update vite config

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div class="main-container">
       <div class="container">
         <section class="main-container__left-side">
-        <img src="soufianeBoutahiri.jpg" alt="Soufiane Boutahiri PhotoS" >
+        <img src="/soufianeBoutahiri.jpg" alt="Soufiane Boutahiri PhotoS" >
         <h1 >Soufiane BOUTAHIRI</h1>
         <nav >
             <RouterLink to="/">Home</RouterLink>
             <RouterLink to="/about">About</RouterLink>
             <RouterLink to="/education">Education</RouterLink>
             <RouterLink to="/technologies">Technologies</RouterLink>
+            <RouterLink to="/projects">Projects</RouterLink>
             <RouterLink to="/contacts">Contacts</RouterLink>
           </nav>
       </section>
