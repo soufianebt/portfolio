1b89b2fe458e265abbcbc1719e7866f6d4bf36f8
soufianebt <soufiane.boutahiri@um5.net.ma>
2023-04-14 18:07:35 +0000

    Update icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import "./assets/scss/main.scss"
 
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {fas} from "@fortawesome/free-solid-svg-icons";
 
