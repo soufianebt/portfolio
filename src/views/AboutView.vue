c344e0fcacc51a88e6129e2094fd019ff756edd9
soufianebt <soufianeboutahiri@gmail.com>
2025-04-25 00:58:55 +0100

    Update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,10 +3,10 @@
         <h1><font-awesome-icon icon="lightbulb" /> About Me</h1>
         <ul>
             <li><span>Full Name :</span> Soufiane BOUTAHIRI</li>
-            <li><span>Age :</span> 24</li>
+            <li><span>Age :</span> 26</li>
             <li><span>Qualification :</span> State Engineer (Bac +5)</li>
             <li><span>Post :</span> Full-Stack Engineer Dot Net/ Vue js</li>
-            <li><span>Languages :</span> Arabic(Native), French(C1), English (B2)</li>
+            <li><span>Languages :</span> Arabic(Native), French(C1), English (C1)</li>
         </ul>
         <button title="Click to download the Resume" @click="downloadFile">Download CV</button>
     </div>
